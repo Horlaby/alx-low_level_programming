ff7960867974739a7320fe134b4747b9e3fb9e73
Haidara Yushau <setunky@gmail.com>
2023-02-16 19:51:14 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * 
+ * Main : This is an entry mode for our code
+ *
+ * Return: with 0 this means success
+ *
+ */
+
+ int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
