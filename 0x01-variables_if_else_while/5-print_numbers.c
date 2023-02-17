2f0d81781a26700917cb07df8af33a6299fe231c
Haidara Yushau <setunky@gmail.com>
2023-02-17 14:29:27 +0000

    C language

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Printing out base 10 digits
+ * Return: Always 0 and it indicates success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	for (; i < 10; i++)
+	{
+		printf("%d", i)
+	}
+	return (0);
+}
