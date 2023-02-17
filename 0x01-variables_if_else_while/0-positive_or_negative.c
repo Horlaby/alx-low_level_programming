1e62918f54b1852f4f1680df67868014a8111e33
Haidara Yushau <setunky@gmail.com>
2023-02-17 02:02:11 -0800

    C language

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,23 +4,21 @@
  *  main - Entry mode of operation
  *  Return: Alwas 0 (success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%i is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		printf("%i is negative\n", n);
 	}
