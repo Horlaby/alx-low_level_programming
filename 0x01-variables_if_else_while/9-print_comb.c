7b5a1cf5927ed85dd5231051ab4c894d67a7b44c
Haidara Yushau <setunky@gmail.com>
2023-02-17 16:09:53 +0000

    C language

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This prints numbers with commas
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
