8f07ea95c29a6e95f8f4e165c5def68729197b97
Haidara Yushau <setunky@gmail.com>
2023-02-17 18:17:02 +0000

    C language

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hunds;
+	int tens;
+	int units;
+
+	for (units = '0'; units <= '9'; units++) /*increment tens*/
+	{
+		for (tens = (units + 1); tens <= '9'; tens++)
+		{
+			for (hunds = (tens + 1); hunds <= '9'; hunds++) /*one's ten+1*/
+			{
+				putchar(units);
+				putchar(tens);
+				putchar(hunds);
+				
+				if (hunds != '7' || tens != '8' || units != '9') /*print commas*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+		
+		return (0);
+}
