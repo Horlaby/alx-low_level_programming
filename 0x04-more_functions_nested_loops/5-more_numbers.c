ac938d21fb14450772ecbbf725f436df9d358fd7
Haidara Yushau <setunky@gmail.com>
2023-02-23 12:59:52 +0000

    Numbers the universe language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14 10x
+ *
+ * Return: void
+ *
+ */
+
+void more_numbers(void)
+{
+	int i = 0, k;
+
+	while (i < 11)
+	{
+		k = 0;
+		for (; k <= 14; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
