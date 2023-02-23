5d37e8f549a4843b4444455e11fc42ecd0de5b30
Haidara Yushau <setunky@gmail.com>
2023-02-23 11:14:12 +0000

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 
 /**
  * mul - This func multiplies two int
- * @a: The first input 
+ * @a: The first input
  * @b: The second input to multiply
  * Return: The result of the multiplied int
  */
 
 int mul(int a, int b)
 {
-	return (a *b);
+	return (a * b);
 }
